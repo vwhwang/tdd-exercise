327bd0abc2e5568b08c5f5a6c0b321bbe8aab2d7
Chris M <chris@adadevelopersacademy.org>
2019-02-20 12:59:00 -0800

    10 is a valid card

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -1,6 +1,6 @@
 # blackjack_score.rb
 
-VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'King', 'Queen', 'Jack']
+VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'King', 'Queen', 'Jack']
 
 def blackjack_score(hand)
 
