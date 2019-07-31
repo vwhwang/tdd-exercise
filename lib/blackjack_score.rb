8e58c035a03779f31746ebc637332ee8fcf484b3
Dan Roberts <dan@adadevelopersacademy.org>
2019-07-31 16:10:16 -0700

    1 -> Ace

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -1,6 +1,6 @@
 # blackjack_score.rb
 
-VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'King', 'Queen', 'Jack']
+VALID_CARDS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
 
 def blackjack_score(hand)
 
